08287197b3faa9ca46d0946a352eb60ee8faef73
Bogdan <prepelitsa.b@gmail.com>
2019-08-06 12:52:18 +0300

    added interface implementation for easier testing in future

--- controllers/pet.go
+++ controllers/pet.go
@@ -11,28 +11,29 @@ import (
 	"net/http"
 )
 
-func (c *Controller) CreatePetHandler() http.HandlerFunc {
+func (c *Controller) CreatePetPostHandler() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == "POST" {
-			err := r.ParseForm()
-			if err != nil {
-				log.Println(err)
-			}
-			pet := &models.Pet{}
-			pet.Name = r.FormValue("nickname")
-			pet.PetType = r.FormValue("pet-type")
-			pet.Breed = r.FormValue("breed")
-			//pet.Age = r.FormValue("age")
-			//pet.Weight = r.FormValue("weight")
-			pet.Gender = r.FormValue("gender")
-			//petpetDescription := r.FormValue("description")
-			err = c.PetStore.RegisterPet(pet)
-			if err != nil {
-				log.Println(err)
-			}
-			http.Redirect(w, r, "/main", 301)
-		} else {
-			utils.GenerateHTML(w, nil, "cabinetPet")
+		err := r.ParseForm()
+		if err != nil {
+			log.Println(err)
 		}
+		pet := &models.Pet{}
+		pet.Name = r.FormValue("nickname")
+		pet.PetType = r.FormValue("pet-type")
+		pet.Breed = r.FormValue("breed")
+		//pet.Age = r.FormValue("age")
+		//pet.Weight = r.FormValue("weight")
+		pet.Gender = r.FormValue("gender")
+		//petpetDescription := r.FormValue("description")
+		err = c.PetStore.RegisterPet(pet)
+		if err != nil {
+			log.Println(err)
+		}
+		http.Redirect(w, r, "/main", 301)
+	}
+}
+func (c *Controller) CreatePetGetHandler() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		utils.GenerateHTML(w, nil, "cabinetPet")
 	}
 }
